7018a8fe6c8424e865c99729e511da73ab43bc1c
Fan Gao <gaofan83@hotmail.com>
2023-02-17 14:18:48 -0800

    Create generate_new_tracks.sh

---
+++ generate_new_tracks.sh
@@ -0,0 +1,13 @@
+#### Install jbrowse following the online instruction https://jbrowse.org/docs/installation.html
+####
+#### Transfer your custom genome/annotation directory to /var/www/html/jbrowse/
+####
+#### Run a preparation script to generate the annotation files for apollo. See a demo below:
+```
+sudo ../bin/prepare-refseqs.pl --fasta Dcor_assembly_v2_200326.fasta 
+sudo ../bin/flatfile-to-json.pl --gff Dcor_assembly_v2_200326.genes.gff3 --trackType CanvasFeatures --trackLabel Dcor_assembly_v2_200326_genes --nameAttributes "ID,Name"
+sudo ../bin/generate-names.pl --tracks Dcor_assembly_v2_200326_genes
+sudo ../bin/flatfile-to-json.pl --gff Dcor_assembly_v2_200326.repeats.gff3 --trackType CanvasFeatures --trackLabel Dcor_assembly_v2_200326_repeats
+sudo ../bin/generate-names.pl --tracks Dcor_assembly_v2_200326_repeats
+```
+#### Transfer the whole directory to /root/jbrowse/ for apollo to host
